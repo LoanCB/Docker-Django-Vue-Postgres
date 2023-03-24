6469ea528c4105e1c716e990cc614c159adab3d3
Loan <loanbillonnet@gmail.com>
2023-03-24 07:59:54 +0100

    add authorization

--- frontend/src/services/caller.js
+++ frontend/src/services/caller.js
@@ -1,7 +1,16 @@
 import axios from 'axios'
 
 const Axios = axios.create({
-    baseURL: 'http://localhost:8000/'
+    baseURL: 'http://localhost:8000/api/'
+})
+
+Axios.interceptors.request.use(request => {
+    const token = localStorage.getItem('access_token')
+
+    if (token)
+        request.headers.Authorization = 'Bearer ' + token
+
+    return request
 })
 
 export default Axios
